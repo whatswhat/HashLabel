cdf08d72546ea94f3d87adba9f97583c5b55e5a6
diego <rapid5218@hotmail.com>
2019-09-18 15:37:13 +0800

    initial commit.

---
+++ HashLabelDemo/HashLabel.swift
@@ -0,0 +1,153 @@
+//
+//  HashLabel.swift
+//  HashLabelDemo
+//
+//  Created by Diego on 2019/8/26.
+//  Copyright © 2019 whatzwhat. All rights reserved.
+//
+//  ------------------
+// | 2019/8/27 - v0.1 第一版
+// | 2019/9/17 - v0.2 移除 GameplayKit 改用 Int.Random
+//  ------------------
+
+import UIKit
+
+extension HashLabel.Mode {
+    
+    var value: String {
+        var number: Int
+        switch self {
+        case .default: // 預設(英文、符號、數字)
+            number = random(from: 33, to: 126)
+        case .no:      // 數字
+            number = random(from: 48, to: 57)
+        case .zh:      // 英文
+            number = random(from: 13312, to: 40911)
+        case .en:      // 中文
+            number = random(from: 65, to: 122)
+            while 90 < number && number < 97 { number = random(from: 65, to: 122) }
+        }
+        guard let unicode = UnicodeScalar(number) else { return self.value }
+        return String(unicode)
+    }
+    
+    private func random(from: Int, to: Int) -> Int {
+        return Int.random(in: from...to)
+    }
+    
+}
+
+
+final class HashLabel: UILabel {
+    
+    enum Mode { case `default`, no, en, zh }
+    
+    enum StartPoint { case first, last }
+    
+    /// DisplayLink
+    private var timer: CADisplayLink?
+    /// 生成方向
+    public var startPoint: StartPoint = .first
+    /// 亂碼模式
+    private(set) var mode: Mode = .default
+    /// 動畫時間
+    private(set) var totalTime: TimeInterval = 3
+    /// 文字
+    private var content: String = ""
+    /// 開始時間
+    private var startTime: TimeInterval = 0
+    /// 進行時間
+    private var progress: TimeInterval = 0
+    /// 動畫變化區間
+    private var timeSection: TimeInterval = 0
+
+}
+
+
+// MARK: - Public Methods.
+extension HashLabel {
+    
+    public func setText(_ text: String, duration: TimeInterval? = nil, mode: Mode? = nil) {
+        resetProperty()
+        recycleDisplayLink()
+        if let duration = duration {
+            totalTime = duration
+        }
+        if let mode = mode {
+            self.mode = mode
+        }
+        content = text
+        startTime = Date.timeIntervalSinceReferenceDate
+        timeSection = totalTime / Double(content.count)
+        addDisplayLink()
+    }
+    
+}
+
+
+// MARK: - Action Methods.
+extension HashLabel {
+
+    private func recycleDisplayLink() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    private func resetProperty() {
+        content = ""
+        startTime = 0
+        progress = 0
+        timeSection = 0
+    }
+    
+    private func addDisplayLink() {
+        timer = CADisplayLink(target: self, selector: #selector(updateValue(timer:)))
+        timer?.add(to: .main, forMode: .default)
+        timer?.add(to: .main, forMode: .tracking)
+    }
+    
+    @objc private func updateValue(timer: Timer) {
+        let now = Date.timeIntervalSinceReferenceDate
+        progress = now - startTime
+        // 結束邏輯
+        if progress >= totalTime {
+            recycleDisplayLink()
+        }
+        // 計算亂碼長度
+        let randomSize = content.count - Int(floor(progress / timeSection))
+        // 準備容器
+        var randomText = ""
+        // 開始產生亂數
+        for _ in stride(from: 0, to: randomSize, by: 1) {
+            randomText += mode.value
+        }
+        // 計算正常字元的長度
+        let textCount = content.count - randomSize
+        // 容器
+        var text = ""
+        // 開始產生正常字元 ...
+        for index in stride(from: 0, to: textCount, by: 1) {
+            switch startPoint {
+            case .first:
+                let index = content.index(content.startIndex, offsetBy: index)
+                text += String(content[index])
+            case .last:
+                // reversed
+                let index = content.index(content.endIndex, offsetBy: -(index + 1))
+                text = String(content[index]) + text
+            }
+        }
+        // 產生結果
+        switch startPoint {
+        case .first:
+            self.text = text + randomText
+        case .last:
+            self.text = randomText + text
+        }
+        
+    }
+
+}
+
+
+
---
+++ HashLabelDemo/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  HashLabelDemo
+//
+//  Created by Diego on 2019/8/27.
+//  Copyright © 2019 whatzwhat. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var segmented: UISegmentedControl!
+    
+    @IBOutlet weak var label: HashLabel!
+    
+    @IBOutlet weak var field: UITextField!
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var sliderLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sliderAction()
+    }
+    
+    private func getMode() -> HashLabel.Mode {
+        print(segmented.selectedSegmentIndex)
+        switch segmented.selectedSegmentIndex {
+        case 1:
+            return .no
+        case 2:
+            return .en
+        case 3:
+            return .zh
+        default:
+            return .default
+        }
+    }
+
+    @IBAction func button(_ sender: UIButton) {
+        guard let text = field.text, !text.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        label.setText(text, duration: Double(slider.value), mode: getMode())
+    }
+    
+    @IBAction func tap(_ sender: UITapGestureRecognizer) {
+        field.endEditing(true)
+    }
+    
+    @IBAction func point(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            label.startPoint = .first
+        default:
+            label.startPoint = .last
+        }
+    }
+    
+    @IBAction func sliderChange(_ sender: UISlider) {
+        sliderAction()
+    }
+    
+    private func sliderAction() {
+        let result = String(format: "%.1f", Double(slider.value))
+        sliderLabel.text = "\(result) 秒"
+    }
+    
+
+}
+
