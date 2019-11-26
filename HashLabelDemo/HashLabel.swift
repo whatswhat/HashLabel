038e3fca1700be6933bfb0b0b8e14e506590568e
diego <rapid5218@hotmail.com>
2019-11-26 10:40:13 +0800

    修正白名單成員依然會被算進總動畫時間之問題

--- HashLabelDemo/HashLabel.swift
+++ HashLabelDemo/HashLabel.swift
@@ -9,6 +9,7 @@
 // | 2019/08/27 - v0.1 第一版
 // | 2019/09/17 - v0.2 移除 GameplayKit 改用 Int.Random
 // | 2019/10/18 - v0.3 增加白名單功能
+// | 2019/11/26 - v0.4 修正白名單成員依然會算進總動畫時間之問題
 //  ------------------
 
 import UIKit
@@ -51,7 +52,7 @@ final class HashLabel: UILabel {
     public var startPoint: StartPoint = .first
     /// 亂碼模式
     private(set) var mode: Mode = .default
-    /// 動畫時間
+    /// 動畫運行時間
     private(set) var totalTime: TimeInterval = 3
     /// 文字
     private var content: String = ""
@@ -63,7 +64,9 @@ final class HashLabel: UILabel {
     private var timeSection: TimeInterval = 0
     /// 白名單
     public var whitelist: [String] = []
-
+    /// 白名單補償時差
+    private var compensationTime : TimeInterval = 0
+    
 }
 
 
@@ -81,8 +84,28 @@ extension HashLabel {
         }
         content = text
         startTime = Date.timeIntervalSinceReferenceDate
+        compensationMultiplier()
         timeSection = totalTime / Double(content.count)
         addDisplayLink()
+    }
+    
+   
+    /* 補償因為白名單的關係, 停滯的動畫時間
+     
+     - 實際的法是, 取得扣除掉白名單的文字長度, 除上總文字長度, 取得相對倍率後再乘上總動畫時間
+     
+     - 並在計算的時候, 補償(跳過)那一段區間的時間, 這樣就同時會相等使用者設定的動畫時間有可以跳過白名單的停滯時間
+     
+     */
+    private func compensationMultiplier() {
+        var actualText = content
+        for white in whitelist {
+            actualText = actualText.filter { char -> Bool in
+                return String(char) != white
+            }
+        }
+        let multiplier = Double(content.count) / Double(actualText.count)
+        totalTime = totalTime * multiplier
     }
     
 }
@@ -101,6 +124,7 @@ extension HashLabel {
         startTime = 0
         progress = 0
         timeSection = 0
+        compensationTime = 0
     }
     
     private func addDisplayLink() {
@@ -111,7 +135,7 @@ extension HashLabel {
     
     @objc private func updateValue(timer: Timer) {
         let now = Date.timeIntervalSinceReferenceDate
-        progress = now - startTime
+        progress = (now - startTime) + compensationTime
         // 結束邏輯
         if progress >= totalTime {
             recycleDisplayLink()
@@ -133,29 +157,42 @@ extension HashLabel {
                 let index = content.index(content.startIndex, offsetBy: index)
                 text = String(content[index])
             }
-            
             if whitelist.contains(text) {
                 randomText += text
             } else {
                 randomText += mode.value
             }
+            
         }
+        // 白名單長度
+        var whitelistCount: Double = 0
         // 計算正常字元的長度
         let textCount = content.count - randomSize
         // 容器
         var text = ""
         // 開始產生正常字元 ...
         for index in stride(from: 0, to: textCount, by: 1) {
+            
+            let textOfIndex: String
             switch startPoint {
             case .first:
                 let index = content.index(content.startIndex, offsetBy: index)
-                text += String(content[index])
+                textOfIndex = String(content[index])
+                text += textOfIndex
             case .last:
                 // reversed
                 let index = content.index(content.endIndex, offsetBy: -(index + 1))
-                text = String(content[index]) + text
+                textOfIndex = String(content[index])
+                text = textOfIndex + text
             }
+            // 累加目前白名單長度
+            if whitelist.contains(textOfIndex) {
+                whitelistCount += 1
+            }
+            
         }
+        // 補償白名單停滯時間
+        compensationTime  = timeSection * whitelistCount
         // 產生結果
         switch startPoint {
         case .first:
@@ -165,6 +202,8 @@ extension HashLabel {
         }
         
     }
+    
+    
 
 }
 
